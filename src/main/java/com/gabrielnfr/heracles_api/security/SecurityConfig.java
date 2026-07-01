4c6a932e831214d0e7b550347b66576c51d45668
Skickle <skicklew@gmail.com>
2026-07-01 14:47:59 -0300

    fix: add actuator endpoints to security config

--- src/main/java/com/gabrielnfr/heracles_api/security/SecurityConfig.java
+++ src/main/java/com/gabrielnfr/heracles_api/security/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .requestMatchers("/", "/h2-console/**").permitAll()
+                .requestMatchers("/actuator/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
